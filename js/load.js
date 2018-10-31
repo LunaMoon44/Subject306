a22754a5a25965a82f1290675cf263ad59063dea
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-10-31 14:28:53 +1100

    stuff around
    Added background (Try and guess what else)

--- js/load.js
+++ js/load.js
@@ -4,7 +4,7 @@ var loadState = {
     loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'}); //text to say loading...
 
     // load sprites
-  
+game.load.image(0, 0, 'background');
 
   },
 
