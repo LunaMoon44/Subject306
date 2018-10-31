ad760950ca31f05dc2e63e368615d46ece1e1920
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-10-31 14:25:47 +1100

    added files

---
+++ js/game.js
@@ -0,0 +1,16 @@
+//Create a Phaser game with dimensions 800x600
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-world');
+
+//Add the different games states
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('room1', );
+game.state.add('win', winState):
+//Any game spanning variables/settings could be placed here
+game.global = {
+  lives: 3
+};
+
+//Load the boot state
+game.state.start('boot');
---
+++ js/load.js
@@ -0,0 +1,15 @@
+var loadState = {
+  preload: function() {
+    //Add a line of text to the screen
+    loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'}); //text to say loading...
+
+    // load sprites
+  
+
+  },
+
+  create: function() {
+  game.state.start('menu');                               // load the menu
+  }
+
+  };
