00d730237d64376046277f9d030678b374e2eb80
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-11-02 10:07:52 +1100

    controls
    added the description of what keys do what

--- js/menu.js
+++ js/menu.js
@@ -6,5 +6,8 @@ var menuState = {
 
 // controls
 help = game.add.text(0, 510, '', {font: '30px Liquid Crystal', fill: '#93FE20'});
+  helpText = 'W A S D keys to move \n';
+  helpText = 'enter to select \n';
+  helpText = 'p to pick up'; 
 }
 };
