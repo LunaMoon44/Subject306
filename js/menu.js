2a19630d6298d7a3dcd6414e5007d4b595604cb2
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-11-05 14:19:01 +1100

    adding more code
    adding button
    adding room1 level state.

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,19 @@ var menuState = {
 // controls
 help = game.add.text(0, 510, '', {font:  "Ink Free", fontSize: "30px", fill: '#93FE20'});
   helpText = 'W A S D keys to move \n';
-  helpText = 'enter to select \n'; 
+  helpText = 'enter to select \n';
   helpText = 'p to pick up';
-}
+
+//button to START
+button = game.add.button(game.world.centerX, game.world.centerY);
+button.anchor.setTo(0.5,0.5);
+button.onInputUp.add(this.start); //When the button is clicked, run the start function
+text = game.add.text(button.x,button.y,'START', {font: '50px Arial', fill: '#00ff00'});  //Some text for the button with arial font coloured green
+text.anchor.setTo(0.5,0.5);
+
+},
+start: function() {
+game.state.start('room1');
+
+},
 };
