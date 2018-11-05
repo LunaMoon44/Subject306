01b235c53e497ee6ef498e79d3b7e68fc5c4259f
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-11-05 14:03:45 +1100

    changing font
    the font is changed from Liquard crystal to Ink free

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,9 @@ var menuState = {
   title = game.add.text(game.world.centerX, game.world.centerY, 'Subject 306', {font: '50px Liquid Crystal', fill: '#93FE20'});
 
 // controls
-help = game.add.text(0, 510, '', {font: '30px Liquid Crystal', fill: '#93FE20'});
+help = game.add.text(0, 510, '', {font:  "Ink Free", fontSize: "30px", fill: '#93FE20'});
   helpText = 'W A S D keys to move \n';
-  helpText = 'enter to select \n';
-  helpText = 'p to pick up'; 
+  helpText = 'enter to select \n'; 
+  helpText = 'p to pick up';
 }
 };
