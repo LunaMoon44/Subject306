11458f651c9241a78ccf2ebb1d5d7564d1e14ae4
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-11-13 13:12:50 +1100

    movement
    added the player to be able to move
                            [Note]
    you will need to add this everytime you
            make a new state/room

--- js/roomOne.js
+++ js/roomOne.js
@@ -4,6 +4,17 @@ var roomStateOne = {
   game.add.sprite(250,31.5, 'bg test');
 
   game.add.sprite(500, 63, 'subject306');
+
+  //player Movements
+  this.controls = game.input.keybored.addkeys(
+    {
+      'up': Phaser.KeyCode.W,
+      'down': Phaser.Keycode.S,
+      'left': Phaser.Keycode.A,
+      'right': Phaser.Keycode.D,
+      'select': Phaser.KeyCode.ENTER
+    }
+  )
   }
 
 };
