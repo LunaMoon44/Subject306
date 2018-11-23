ac40940ef8faa5f931097dc8b9d7f337226cabe6
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-11-23 17:20:54 +1100

    animation
    added other animations and fixed left and right animations.
    
    added the animations to movement

--- js/roomOne.js
+++ js/roomOne.js
@@ -8,11 +8,14 @@ var roomStateOne = {
   subject306.body.collideWorldBounds = true; // want to make it the background bounds
   subject306.body.immovable = true;
 
-player.animation.add('left', [6, 7, 6, 8], 10, true);
-palyer.animation.add('right', [9, 10, 9, 11], 10, true);
+player.animation.add('left', [9, 10, 9, 11], 10, true);
+palyer.animation.add('right', [6, 7, 6, 8], 10, true);
 player.animation.add('forward', [0, 1, 0, 2], 10, true);
-player.animation.add('back', [4, 5, 4, 6], 10, true);
+player.animation.add('back', [3, 4, 3, 5], 10, true);
 player.animation.add('still front', [0], 1, false);
+player.animation.add('still back', [3], 1, false);
+player.animation.add('still left', [9], 1, false);
+player.animation.add('still right', [6], 1, false);
 
 game.add.sprite(550, 66, 'key');
 game.add.sprite(550, 500, 'fakekey');
@@ -35,18 +38,22 @@ update: function() {
 //player Movements
 if(this.controls.up.isDown){
   subject306.body.velocity.y = -150;
+player.animation.play('back');
 }
 else if(this.controls.down.isDown){
   subject306.body.velocity.y = 150;
+  player.animation.play('forward');
 }
 else {
   subject306.body.velocity.y = 0;
 }
 if(this.controls.left.isDown) {
   subject306.body.velocity.x = -150;
+  player.animation.play('left');
 }
 else if(this.controls.right.isDown){
   subject306.body.velocity.x = 150;
+  play.animation.play('right');
 }
 else {
   subject306.body.velocity.x = 0;
