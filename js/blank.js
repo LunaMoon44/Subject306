799efac8488e56f5250866a0045a2d639a191593
LunaMoon44 <44593958+LunaMoon44@users.noreply.github.com>
2018-12-02 20:46:29 +1100

    sounds and change state
    added audio for a water effect and added a select action so when the sound ends you press enter and will go to the  next state

--- js/blank.js
+++ js/blank.js
@@ -4,16 +4,26 @@ create: function() {
 bg = game.add.sprite(0, 0, 'blackroom');
 Water = game.add.audio('Water', 1, true);
 Water.play();
-bg.time.events.add(Phaser.Timer.SECOND*10, loadnext, this);
+
+this.controls = game.input.keyboard.addKeys(  //adding perpouse for keys
+  {
+     'up1': Phaser.KeyCode.UP,                //the UP key is now 'up1'
+     'down1': Phaser.KeyCode.DOWN,            //the DOWN key is now 'down1'
+     'up2': Phaser.KeyCode.W,                 //the W key is now 'up2'
+     'down2': Phaser.KeyCode.S,               //the S key is now 'down2'
+     'select': Phaser.KeyCode.ENTER         //the ENTER is now 'select'
+  }
+)
+
 
 },
 
 update: function() {
-game.debug.text("time until next level: "+ bg.time.events.duration,32,32);
-},
 
-loadnext: function(){
-  console.log('next');
-    game.state.start('forrest');
+if(this.controls.select.isDown){
+game.state.start('forest');  
 }
+
+}
+
 };
