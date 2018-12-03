69e4105c9458f9ea559fd08bb9c152ca098108cc
Jewel East <44594175+99cStore@users.noreply.github.com>
2018-12-03 17:48:32 +1100

    Added sprites Changed Font Colour
    IN this one I added the fully done Menu background, changed the colour of the font on the screen and cleaned up the Broken Floor Sprite.

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,11 @@ var menuState = {
   // title
   bg = game.add.sprite(game.world.width/2,game.world.height/2, 'menu bg');
   bg.anchor.setTo(0.5);
-  
-  title = game.add.text(500, 200, 'Subject 306', {font: '50px Orbitron', fill: '#93FE20'});
+
+  title = game.add.text(500, 200, 'Subject 306', {font: '50px Orbitron', fill: '#4b6d34'});
 
 // controls
-help = game.add.text(100, 450, '', {font:  "Special Elite", fontSize: "30px", fill: '#93FE20'});
+help = game.add.text(100, 450, '', {font:  "Special Elite", fontSize: "30px", fill: '#4b6d34'});
   helpText = 'W A S D keys to move \n';
   helpText += 'enter to select \n';
   helpText += 'p to pick up';
@@ -18,7 +18,7 @@ button = game.add.button(game.world.centerX-100, game.world.centerY-50);
 button.scale.setTo(6, 2);
 //button.anchor.setTo(0.5,0.5);
 button.onInputUp.add(this.start); //When the button is clicked, run the start function
-text = game.add.text(button.x,button.y,'START', {font: '50px Orbitron', fill: '#00ff00'});  //Some text for the button with arial font coloured green
+text = game.add.text(button.x,button.y,'START', {font: '50px Orbitron', fill: '#548235'});  //Some text for the button with arial font coloured green
 //text.anchor.setTo(0.5,0.5);
 
 },
